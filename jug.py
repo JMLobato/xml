868f3ab129bc41d97c7be123b69422e3237ede85
JMLobato <derc8aid@gmail.com>
2018-03-05 09:28:36 +0100

    1º apartado terminado

--- jug.py
+++ jug.py
@@ -2,4 +2,10 @@ from lxml import etree
 doc=etree.parse('jugueteria.xml')
 raiz=doc.getroot()
 nombres=raiz.xpath("product/name")
-print(nombres)
+precio=raiz.xpath("product/price")
+iva=raiz.xpath("product/iva")
+for x,y,z in zip(nombres,precio,iva):
+	total=float(y.text)+float(z.text)
+	print("Articulo:",x.text)
+	print("Precio Total:",total)
+	print()
