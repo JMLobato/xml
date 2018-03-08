0f97a34c227f520f236678724158e8d4821500c7
JMLobato <derc8aid@gmail.com>
2018-03-08 09:46:01 +0100

    5º apartado terminado

--- jug.py
+++ jug.py
@@ -6,7 +6,7 @@ nombres=raiz.xpath("product/name")
 precio=raiz.xpath("product/price")
 iva=raiz.xpath("product/iva")
 for x,y,z in zip(nombres,precio,iva):
-	total=float(y.text)+float(z.text)
+	total=float(y.text)*(float(z.text)/100)
 	print("Artículo:",x.text)
 	print("Precio Total:",total)
 	print()
@@ -30,6 +30,21 @@ fallo=False
 for elem in nombres:
 	if (articulo).upper() == (elem.text).upper():
 		print("Artículos en stock:",stock[nombres.index(elem)].text)
+		fallo=True
+if fallo != True:
+	print("Ese artículo no existe")
+#5º apartado
+articulo=input("Introduce un artículo: ")
+fallo=False
+for elem in nombres:
+	if (articulo).upper() == (elem.text).upper():
+		print("Descripción:",raiz.xpath("product/description")[nombres.index(elem)].text)
+		print("Precio sin IVA:",raiz.xpath("product/price")[nombres.index(elem)].text)
+		print("IVA:",raiz.xpath("product/iva")[nombres.index(elem)].text)
+		print("Artículos en stock:",raiz.xpath("product/stock")[nombres.index(elem)].text)
+		print("Primera fecha:",raiz.xpath("product/date_add")[nombres.index(elem)].text)
+		print("Ultima fecha:",raiz.xpath("product/date_upd")[nombres.index(elem)].text)
+		print()
 		fallo=True
 if fallo != True:
 	print("Ese artículo no existe")
